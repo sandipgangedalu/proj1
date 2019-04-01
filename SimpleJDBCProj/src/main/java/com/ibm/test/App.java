972c5de39a2c225dd6a1d3bd2719934c6c100667
Sandip Gangedalu <Sandip Gangedalu@DESKTOP-PMVAM1A>
2019-04-01 08:27:00 +0530

    tl-added project

--- SimpleJDBCProj/src/main/java/com/ibm/test/App.java
+++ SimpleJDBCProj/src/main/java/com/ibm/test/App.java
@@ -8,6 +8,11 @@ public class App
 {
     public static void main( String[] args )
     {
-        
+        try {
+			Class.forName("oracle driver");
+		} catch (ClassNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
     }
 }
