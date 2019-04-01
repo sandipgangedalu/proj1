5ce67c10215975e55b759b9ae66394bf24716c78
Sandip Gangedalu <Sandip Gangedalu@DESKTOP-PMVAM1A>
2019-04-01 08:44:19 +0530

    dev1-dao

--- SimpleJDBCProj/src/main/java/com/ibm/test/App.java
+++ SimpleJDBCProj/src/main/java/com/ibm/test/App.java
@@ -1,16 +1,33 @@
 package com.ibm.test;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
 /**
  * Hello world!
  *
  */
-public class App 
-{
-    public static void main( String[] args )
+public class App {
+	public static void main( String[] args )
     {
+    	Connection con =null;
+    	PreparedStatement ps=null;
+    	ResultSet rs=null;
         try {
-			Class.forName("oracle driver");
-		} catch (ClassNotFoundException e) {
+			Class.forName("oracle.jdbc.driver.OracleDriver");
+			con=DriverManager.getConnection("jdbc:oracle:thin:@localhost:1521:xe", "system", "system");
+			ps=con.prepareStatement("SELECT COUNT(*) FROM EMP");
+		rs=	ps.executeQuery();
+			rs.next();
+			System.out.println(rs.getInt(1));
+		
+        } catch (ClassNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (SQLException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
